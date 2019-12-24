6c2ffef6bb8c1cd04dba5b19c60217f78065d02f
Ivana <ivana.zaccheddu88@gmail.com>
2019-12-24 10:27:29 +0100

    collegati template login e signup e aggiunto lo stesso stile

--- FlaskFB5/app.py
+++ FlaskFB5/app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/main")
 def main():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
