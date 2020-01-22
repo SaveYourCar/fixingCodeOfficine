6361f81cdc5de7eb7e6d313f80991f8eff4d8d18
Ivana <ivana.zaccheddu88@gmail.com>
2020-01-22 14:44:34 +0100

    add working flask API and carousel

---
+++ working_code/esempio carousel/js/ajax.js
@@ -0,0 +1,81 @@
+
+ function get(url, successCallback, errorCallback) {
+    // istanzio l'oggetto request
+    var xhr = new XMLHttpRequest();
+  
+    // creo un domain request per non incappare nell'eccezione CORS
+    if (!("withCredentials" in xhr)) xhr = new XDomainRequest();
+  
+    // apro la connessione per effettuare la GET all'url specificato
+    xhr.open("GET", url, true);
+ 
+ // dichiaro la callback da usare in caso di successo
+  xhr.onload = successCallback;
+
+  // dichiaro la callback da usare in caso di errore
+  xhr.onerror = errorCallback;
+
+  // effettuo la chiamata
+  xhr.send();
+  return xhr;
+}
+
+function post(url, data, successCallback, errorCallback) {
+  /* 
+ // questo va a ggiunto nel caso in cui i dati arrivino direttamente un form html
+ var params =
+    typeof data == "string"
+      ? data
+      : Object.keys(data)
+          .map(function(k) {
+            return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+          })
+          .join("&");
+*/
+  var params = data;
+
+  // istanzio l'oggetto request
+  var xhr = new XMLHttpRequest();
+
+  // creo un domain request per non incappare nell'eccezione CORS
+  if (!("withCredentials" in xhr)) xhr = new XDomainRequest();
+
+  // preparo la chiamata all'url specificato
+  xhr.open("POST", url);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState > 3) {
+        
+      if (xhr.status == 201) {
+        // dichiaro la callback nel caso di successo
+        successCallback(xhr.responseText);
+      }
+      // dichiaro la callback nel caso di fallimento
+      else errorCallback();
+    }
+  };
+
+  // setto gli header necessari
+  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // effettuo la chiamata
+  xhr.send(params);
+  return xhr;
+}
+
+function errorHandler() {
+  console.log("XHR ERROR!!!");
+}
+
+
+/*
+post(
+  "https://reqres.in/api/users",
+  '{"name": "Mirko Peloso","job": "docente"}',
+  function(data) {
+    console.log("post result : ", data);
+  },
+  errorHandler
+);
+*/
---
+++ working_code/restapiflask/README.md
@@ -0,0 +1,19 @@
+#REST API With Flask & SQL Alchemy
+
+questo progetto è l'ho preso da un tutorial
+non sono riusciuto a creare il db.
+mi da sempre un errore su:
+
+from flask_marshmallow import Marshmallow
+
+non ostante abbia installato flask-marshmallow e 
+marshmallow-sqlalchemy
+
+questi sono i link del progetto:
+https://www.youtube.com/watch?v=PTZiDnuC86g
+
+https://github.com/bradtraversy/flask_sqlalchemy_rest/blob/master/app.py
+
+vedi se ti fa creare il db
+
+grazie
