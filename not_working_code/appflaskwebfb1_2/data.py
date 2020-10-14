d4fc2fb7549df3eab81fe8222a2ecfbff5d706ba
filippo <basciu45@gmail.com>
2020-10-14 16:47:20 +0200

    inserito appflaskwebfb1_2

---
+++ not_working_code/appflaskwebfb1_2/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+    {
+    'id':1,
+    'title':'Article one',
+    'body':'Quando il bambino entra nel mondo della scuola, e in particolare nel grado preparatorio fascia 3/6 anni la musica diventa uno strumento necessario e indispensabile',
+    'author':'Filippo',
+    'create_date':'08-10-2020',
+    },
+    {
+    'id':2,
+    'title':'Article two',
+    'body':'La musica è una forma comunicativa complessa e “globale”: essa racchiude e veicola un’infinità di significati perché è strettamente legata all’immaginazione e al pensiero',  
+    'author':'Filippo',
+    'create_date':'07-10-2020',
+    },
+    {
+    'id':3,
+    'title':'Article three',
+    'body':'Beethoven, celebre compositore e pianista tedesco, amava definire la musica come “una rivelazione. La più alta, più di qualsiasi saggezza e di qualsiasi filosofia. Vale, anzi, tutte le filosofie del mondo”.',
+    'author':'Filippo',
+    'create_date':'06-10-2020',
+    }
+    ]
+
+    return articles
